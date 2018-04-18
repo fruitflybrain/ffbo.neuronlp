388240cf48256a77506524afcb391ef5028982e3
Nikul <nikul@ee.columbia.edu>
2018-04-18 12:53:34 -0400

    Start writing app for integration of InfoPanel and Client

--- test_site/app.js
+++ test_site/app.js
@@ -6,7 +6,7 @@ define('modernizr', [], Modernizr);
 // directory.
 requirejs.config({
     paths: {
-        app: '../app',
+        app: 'app',
         'autobahn': '//cdn.rawgit.com/crossbario/autobahn-js-built/master/autobahn.min',
         'd3':'lib/d3.min',
         'jquery':'//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
@@ -22,14 +22,29 @@ requirejs([
 ],
 function (
   $,
-  Client,
+  FFBOClient,
   InfoPanel,
 ){
+  var infoPanel;
   $.getJSON('./data/data.json',function(d){
     var synData = d['success']['data']["synaptic_info_2"];
     var neuData = d['success']['data']["summary_2"];
+    infoPanel = new InfoPanel("#info-panel", neuData, synData);
 
-    var infoPanel = new InfoPanel("#info-panel",neuData,synData);
-  });
+  
+  }); 
+  
+  //var infoPanel = new InfoPanel("#info-panel");
+
+  var client = new FFBOClient();
+  client.startConnection("guest", "guestpass", "wss://neuronlp.fruitflybrain.org:8888/ws")
+
+  window["fetchNeuronInfo"] = function(rid){
+    client.executeNAquery(client.neuronInfoQuery(rid), {success: infoPanel.update})
+  }
+  window["fetchSynapseInfo"] = function(rid){
+    client.executeNAquery(client.synapseInfoQuery(rid), {success: infoPanel.update})
+  }
+
 });
 
