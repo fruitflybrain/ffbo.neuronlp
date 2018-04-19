00631007bba0c1a139d34d13b9c31c4bc52c9325
Nikul <nikul@ee.columbia.edu>
2018-04-19 15:09:37 -0400

    Started to include THREE.js dependencies in app.js

--- test_site/app.js
+++ test_site/app.js
@@ -1,29 +1,49 @@
 // For any third party dependencies, like jQuery, place them in the lib folder.
-define('modernizr', [], Modernizr);
+//define('modernizr', [], Modernizr);
 
 // Configure loading modules from the lib directory,
 // except for 'app' ones, which are in a sibling
 // directory.
+
+define('three', ['https://cdn.rawgit.com/mrdoob/three.js/r92/build/three.min.js'], function(THREE){
+  window.THREE = THREE;
+  return THREE;
+});
 requirejs.config({
-    paths: {
-        app: 'app',
-        'autobahn': '//cdn.rawgit.com/crossbario/autobahn-js-built/master/autobahn.min',
-        'd3':'lib/d3.min',
-        'jquery':'//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
-    }
+  paths: {
+    app: 'app',
+    autobahn: '//cdn.rawgit.com/crossbario/autobahn-js-built/master/autobahn.min',
+    d3: 'lib/d3.min',
+    jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+    detector: 'https://cdn.rawgit.com/mrdoob/three.js/r92/examples/js/Detector',
+    trackball: 'https://cdn.rawgit.com/mrdoob/three.js/r92/examples/js/controls/TrackballControls',
+    modernizr: "lib/modernizr"
+  },
+  shim: {
+    modernizr: {exports: 'Modernizr'},
+    detector: {deps: ['three'], exports: 'Detector'},
+    trackball: {deps: ['three'], init: function(THREE){
+      return THREE.TrackballControls;
+    }},
+  }
 });
 
 // Start loading the main app file. Put all of
 // your application logic in there.
-requirejs([
+require([
   'jquery',
   'app/Client',
-  'app/info_panel/info_panel'
-],
-function (
-  $,
-  FFBOClient,
-  InfoPanel,
+  'app/info_panel/info_panel',
+  'three',
+  'detector',
+  'trackball'
+], function (
+   $,
+   FFBOClient,
+   InfoPanel,
+   THREE,
+   Detector,
+   Trackball
 ){
   var infoPanel;
   $.getJSON('./data/data.json',function(d){
@@ -31,9 +51,11 @@ function (
     var neuData = d['success']['data']["summary_2"];
     infoPanel = new InfoPanel("#info-panel", neuData, synData);
 
-  
-  }); 
-  
+
+  });
+  console.log(THREE)
+  console.log(Detector)
+  console.log(Trackball)
   //var infoPanel = new InfoPanel("#info-panel");
 
   var client = new FFBOClient();
@@ -47,4 +69,3 @@ function (
   }
 
 });
-
