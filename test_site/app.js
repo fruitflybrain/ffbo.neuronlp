0d0445b15bd80c9efba33212e1eab893e9bced41
Chung-Heng Yeh <chungheng.yeh@gmail.com>
2018-04-19 19:11:57 -0400

    incorporate mesh3d in to test_site

--- test_site/app.js
+++ test_site/app.js
@@ -10,53 +10,122 @@ define('three', ['https://cdn.rawgit.com/mrdoob/three.js/r92/build/three.min.js'
   return THREE;
 });
 requirejs.config({
+  baseUrl: '../js',
   paths: {
-    app: 'app',
+    // app: 'app',
+    mesh3d: '//cdn.rawgit.com/fruitflybrain/ffbo.lib/VisualizationUpdates/js/mesh3d',
     autobahn: '//cdn.rawgit.com/crossbario/autobahn-js-built/master/autobahn.min',
     d3: 'lib/d3.min',
     jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
-    detector: 'https://cdn.rawgit.com/mrdoob/three.js/r92/examples/js/Detector',
-    trackball: 'https://cdn.rawgit.com/mrdoob/three.js/r92/examples/js/controls/TrackballControls',
+    detector: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/Detector',
+    simplifymodifier: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/utils/SceneUtils',
+    lut: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/math/Lut',
+    copyshader: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/shaders/CopyShader',
+    convolutionshader: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/shaders/ConvolutionShader',
+    fxaashader: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/shaders/FXAAShader',
+    ssaoshader: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/shaders/SSAOShader',
+    luminosityhighpassshader: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/shaders/LuminosityHighPassShader',
+    luminosityshader: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/shaders/LuminosityShader',
+    tonemapshader: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/shaders/ToneMapShader',
+    gammacorrectionshader: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/shaders/GammaCorrectionShader',
+    effectcomposer: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/postprocessing/EffectComposer',
+    renderpass: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/postprocessing/RenderPass',
+    ssaarenderpass: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/postprocessing/SSAARenderPass',
+    shaderpass: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/postprocessing/ShaderPass',
+    ssaopass: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/postprocessing/SSAOPass',
+    maskpass: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/postprocessing/MaskPass',
+    bloompass: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/postprocessing/BloomPass',
+    unrealbloompass: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/postprocessing/UnrealBloomPass',
+    adaptivetonemappingpass: '//cdn.rawgit.com/mrdoob/three.js/r92/examples/js/postprocessing/AdaptiveToneMappingPass',
+    trackballcontrols: '//cdn.rawgit.com/fruitflybrain/ffbo.lib/VisualizationUpdates/js/three/libs/TrackballControls',
+    lightshelper: '//cdn.rawgit.com/fruitflybrain/ffbo.lib/VisualizationUpdates/js/lightshelper',
     modernizr: "lib/modernizr"
   },
   shim: {
     modernizr: {exports: 'Modernizr'},
     detector: {deps: ['three'], exports: 'Detector'},
-    trackball: {deps: ['three'], init: function(THREE){
-      return THREE.TrackballControls;
-    }},
+    trackballcontrols: {deps: ['three']},
+    simplifymodifier: {deps: ['three']},
+    lut: {deps: ['three']},
+    copyshader: {deps: ['three']},
+    convolutionshader: {deps: ['three']},
+    fxaashader: {deps: ['three']},
+    ssaoshader: {deps: ['three']},
+    luminosityhighpassshader: {deps: ['three']},
+    luminosityshader: {deps: ['three']},
+    tonemapshader: {deps: ['three']},
+    gammacorrectionshader: {deps: ['three']},
+    effectcomposer: {deps: ['three']},
+    renderpass: {deps: ['three']},
+    ssaarenderpass: {deps: ['three']},
+    shaderpass: {deps: ['three']},
+    ssaopass: {deps: ['three']},
+    maskpass: {deps: ['three']},
+    bloompass: {deps: ['three']},
+    unrealbloompass: {deps: ['three']},
+    adaptivetonemappingpass: {deps: ['three']},
+    lightshelper: {deps: ['three']}
   }
 });
+
+
+
 
 // Start loading the main app file. Put all of
 // your application logic in there.
 require([
   'jquery',
-  'app/Client',
-  'app/info_panel/info_panel',
+  'Client',
   'three',
   'detector',
-  'trackball'
+  'mesh3d'
 ], function (
    $,
    FFBOClient,
-   InfoPanel,
    THREE,
    Detector,
-   Trackball
+   FFBOMesh3D
 ){
   var infoPanel;
+
+  var lpuList = [
+      'al_l', 'al_r', 'ammc_l', 'ammc_r', 'cal_l', 'cal_r', 'ccp_l', 'ccp_r',
+      'cmp_l', 'cmp_r', 'cvlp_l', 'cvlp_r', 'dlp_l', 'dlp_r', 'dmp_l', 'dmp_r',
+      'eb', 'fb', 'fspp_l', 'fspp_r', 'idfp_l', 'idfp_r', 'idlp_l', 'idlp_r',
+      'lat_l', 'lat_r', 'lh_l', 'lh_r', 'lob_l', 'lob_r', 'lop_l', 'lop_r',
+      'mb_l', 'mb_r', 'med_l', 'med_r', 'nod_l', 'nod_r', 'og_l', 'og_r',
+      'optu_l', 'optu_r', 'pan_l', 'pan_r', 'pb', 'sdfp_l', 'sdfp_r', 'sog_l',
+      'sog_r', 'spp_l', 'spp_r', 'vlp_l', 'vlp_r', 'vmp_l', 'vmp_r'
+  ];
+
+  lpuJSON = {}
+  for (var i=0; i < lpuList.length; i++ ) {
+    var x = lpuList[i].split("_");
+    var side = "";
+      if (x.length > 1) {
+        if (x[1] == "r")
+          side = "Right "
+        else
+          side = "Left "
+      }
+      lpuJSON[lpuList[i]] = {
+      'filename': 'https://raw.githubusercontent.com/fruitflybrain/ffbo.lib/master/mesh/' + lpuList[i] + '.json',
+      'label': side + x[0].toUpperCase(),
+      'highlight': false,
+      'background': true,
+      'color': new THREE.Color( 0.15, 0.01, 0.15)
+    };
+  }
+
+  var ffbomesh = new FFBOMesh3D('vis-3d', {"ffbo_json": lpuJSON, "showAfterLoadAll": true}, {"globalCenter": {'x': 0, 'y':-250, 'z':0}});
+
   $.getJSON('./data/data.json',function(d){
     var synData = d['success']['data']["synaptic_info_2"];
     var neuData = d['success']['data']["summary_2"];
     infoPanel = new InfoPanel("#info-panel", neuData, synData);
-
-
   });
-  console.log(THREE)
-  console.log(Detector)
-  console.log(Trackball)
-  //var infoPanel = new InfoPanel("#info-panel");
+
+  // var infoPanel = new InfoPanel("#info-panel");
 
   var client = new FFBOClient();
   client.startConnection("guest", "guestpass", "wss://neuronlp.fruitflybrain.org:8888/ws")
