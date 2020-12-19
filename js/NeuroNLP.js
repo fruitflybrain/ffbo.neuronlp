9e16764f427ef7ca06a4da2f61c31b4c7c7ddf64
Yiyin Zhou <yiyin@ee.columbia.edu>
2020-12-19 03:18:35 -0500

    add notifications for connection and lost connection

--- js/NeuroNLP.js
+++ js/NeuroNLP.js
@@ -270,8 +270,13 @@ require([
         iziToast.success({message: message})
     }
 
-  client.notifyError = function(message){
-    iziToast.error({message: message, timeout: false});
+  client.notifyError = function(message, timeout){
+    if(timeout == undefined){
+        iziToast.error({message: message, timeout: false});
+    }
+    else{
+        iziToast.error({message: message, timeout: timeout});
+    }
     $('#ui-blocker').hide();
   }
 
