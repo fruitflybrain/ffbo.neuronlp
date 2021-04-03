f8319e905f6cc2fce333077d562a99f9e2e19953
mkturkcan <mehmetkeremturkcan@gmail.com>
2021-04-03 15:42:43 -0400

    Fix client url

--- js/NeuroNLP.js
+++ js/NeuroNLP.js
@@ -167,7 +167,7 @@ require([
     }
 
 
-    client.startConnection("guest", "guestpass", "wss://neuronlp.fruitflybrain.org/ws");
+    client.startConnection("guest", "guestpass", "ws://localhost:8081/ws");
 
     //ffbomesh.settings.neuron3d = 1;
     function dataCallback(data) {
